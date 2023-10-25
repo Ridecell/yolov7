1e6c34f39988ef5f213542637b113a9e376a8532
abhilash <abhilash@ridecell.com>
2023-10-25 15:59:24 -0700

    modifications to enable test to run in train

--- yolov7/test.py
+++ yolov7/test.py
@@ -254,7 +254,9 @@ def test(data,
     # Save JSON
     if save_json and len(jdict):
         w = Path(weights[0] if isinstance(weights, list) else weights).stem if weights is not None else ''  # weights
-        anno_json = '../coco/annotations/instances_val2017.json'  # annotations json
+        current_path = os.path.abspath(__file__)
+        parent_path = f"{os.path.dirname(os.path.dirname(current_path))}" #assumes coco is downloaded in the root folder of the repo
+        anno_json = f'{parent_path}/coco/annotations/instances_val2017.json'  # annotations json
         pred_json = str(save_dir / f"{w}_predictions.json")  # predictions json
         print('\nEvaluating pycocotools mAP... saving %s...' % pred_json)
         with open(pred_json, 'w') as f:
