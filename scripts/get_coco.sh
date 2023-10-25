f2088097f0370358e772137cd564ed7f4e20a4bb
Abhilash Balachandran <abhilashbalachandran94@gmail.com>
2023-10-25 13:05:11 -0700

    Update scripts/get_coco.sh
    Co-authored-by: Arpan Chakraborty <napratin@yahoo.co.in>

--- scripts/get_coco.sh
+++ scripts/get_coco.sh
@@ -10,7 +10,7 @@ echo 'Downloading' $url$f ' ...'
 curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
 
 # Download/unzip images
-d='./yolov7/coco/images' # unzip directory
+d='./coco/images' # unzip directory
 url=http://images.cocodataset.org/zips/
 f1='train2017.zip' # 19G, 118k images
 f2='val2017.zip'   # 1G, 5k images
